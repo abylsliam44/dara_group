9c052825f5d6ef0bfe17bc44ce15274f91c8b580
RamBamB00 <ramazan.abitaiev@mail.ru>
2024-09-27 18:17:30 +0500

    add Ramazanfile

--- src/CandyStoreApp.java
+++ src/CandyStoreApp.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class CandyStoreApp {
+import java.util.List;
+import java.util.Arrays;
+
+ interface Candy {
+    String getName();
+    double getPrice();
+}
+
+class Lollipop implements Candy {
+    @Override
+    public String getName() {
+        return "Lollipop";
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.50;
+    }
+}
+
+class Marshmallow implements Candy {
+    @Override
+    public String getName() {
+        return "Marshmallow";
+    }
+
+    @Override
+    public double getPrice() {
+        return 2.00;
+    }
+}
+
+class CandyStore {
+    private List<Candy> candyInventory;
+
+    public CandyStore(List<Candy> candyInventory) {
+        this.candyInventory = candyInventory;
+    }
+
+    public void displayCandy() {
+        for (Candy candy : candyInventory) {
+            System.out.println(candy.getName() + ": $" + candy.getPrice());
+        }
+    }
+
+    public double calculateTotalCost() {
+        return candyInventory.stream().mapToDouble(Candy::getPrice).sum();
+    }
+}
+//skibidi toilet
+public class CandyStoreApp {
+    public static void main(String[] args) {
+        List<Candy> candies = Arrays.asList(new Lollipop(), new Marshmallow());
+        CandyStore store = new CandyStore(candies);
+
+        store.displayCandy();
+        System.out.println("Total cost: $" + store.calculateTotalCost());
+    }
 }

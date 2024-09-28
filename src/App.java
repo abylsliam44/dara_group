3933369101fe3ec4f9e77ab190dc109c60a04270
Daniil <naumenko_0606@mail.ru>
2024-09-28 21:14:44 +0500

    add Daniil files

--- src/App.java
+++ src/App.java
@@ -1,39 +1,3 @@
-public interface Engine {
-    void start();
-}
-
-public class PetrolEngine implements Engine {
-    @Override
-    public void start() {
-        System.out.println("Petrol engine started");
-    }
-}
-
-public class ElectricEngine implements Engine {
-    @Override
-    public void start() {
-        System.out.println("Electric engine started");
-    }
-}
-
-public interface Vehicle {
-    void drive();
-}
-
-public class Car implements Vehicle {
-    private Engine engine;
-
-    public Car(Engine engine) {
-        this.engine = engine;
-    }
-
-    @Override
-    public void drive() {
-        engine.start();
-        System.out.println("Car is driving");
-    }
-}
-
 public class App {
     public static void main(String[] args) {
         Engine petrolEngine = new PetrolEngine();

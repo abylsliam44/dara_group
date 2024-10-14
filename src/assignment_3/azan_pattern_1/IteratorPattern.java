61c8ebd310fe7ff62a6c2797705230a8732d3050
azanito <azan.orunbek@gmail.com>
2024-10-14 20:09:04 +0500

    fixing code

---
+++ src/assignment_3/azan_pattern_1/IteratorPattern.java
@@ -0,0 +1,37 @@
+
+interface Iterator {
+    boolean hasNext();
+    int next();
+}
+
+
+class NumberIterator implements Iterator {
+    private int[] numbers;
+    private int index;
+
+    public NumberIterator(int[] numbers) {
+        this.numbers = numbers;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < numbers.length;
+    }
+
+    @Override
+    public int next() {
+        return numbers[index++];
+    }
+}
+
+public class IteratorPattern {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        NumberIterator iterator = new NumberIterator(numbers);
+
+        while (iterator.hasNext()) {
+            System.out.println("Number: " + iterator.next());
+        }
+    }
+}
---
+++ src/assignment_3/azan_pattern_2/StrategyPattern.java
@@ -0,0 +1,47 @@
+// Strategy Interface
+interface OperationStrategy {
+    int execute(int a, int b);
+}
+
+// Concrete Strategies
+class AddStrategy implements OperationStrategy {
+    @Override
+    public int execute(int a, int b) {
+        return a + b;
+    }
+}
+
+class SubtractStrategy implements OperationStrategy {
+    @Override
+    public int execute(int a, int b) {
+        return a - b;
+    }
+}
+
+// Context Class
+class Calculator {
+    private OperationStrategy strategy;
+
+    public void setStrategy(OperationStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public int calculate(int a, int b) {
+        return strategy.execute(a, b);
+    }
+}
+
+// Client Code
+public class StrategyPattern {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        // Use add strategy
+        calculator.setStrategy(new AddStrategy());
+        System.out.println("10 + 5 = " + calculator.calculate(10, 5));
+
+        // Use subtract strategy
+        calculator.setStrategy(new SubtractStrategy());
+        System.out.println("10 - 5 = " + calculator.calculate(10, 5));
+    }
+}

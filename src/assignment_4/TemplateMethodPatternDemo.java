93ea8ee55c6e8d1ceb1dcb7361810fea396d92a8
RamBamB00 <153795079+RamBamB00@users.noreply.github.com>
2024-10-21 20:10:33 +0500

    Update TemplateMethodPatternDemo.java

--- src/assignment_4/TemplateMethodPatternDemo.java
+++ src/assignment_4/TemplateMethodPatternDemo.java
@@ -1,58 +1,59 @@
-abstract class Game {
+abstract class Recipe {
     // Template method
-    public final void play() {
-        initializeGame();
-        startPlay();
-        endPlay();
+    public final void prepare() {
+        gatherIngredients();
+        cook();
+        serve();
     }
 
-    protected abstract void initializeGame();
-    protected abstract void startPlay();
-    protected abstract void endPlay();
+    protected abstract void gatherIngredients();
+    protected abstract void cook();
+    protected abstract void serve();
 }
 
-class Cricket extends Game {
+class PastaRecipe extends Recipe {
     @Override
-    protected void initializeGame() {
-        System.out.println("Cricket Game Initialized! Start playing.");
+    protected void gatherIngredients() {
+        System.out.println("Ingredients gathered: pasta, tomato sauce, spices.");
     }
 
     @Override
-    protected void startPlay() {
-        System.out.println("Cricket Game Started. Enjoy the game!");
+    protected void cook() {
+        System.out.println("Pasta is boiling, then sauce is added.");
     }
 
     @Override
-    protected void endPlay() {
-        System.out.println("Cricket Game Finished!");
+    protected void serve() {
+        System.out.println("Pasta is served on the table with parmesan.");
     }
 }
 
-class Football extends Game {
+class SaladRecipe extends Recipe {
     @Override
-    protected void initializeGame() {
-        System.out.println("Football Game Initialized! Start playing.");
+    protected void gatherIngredients() {
+        System.out.println("Ingredients gathered: lettuce, tomatoes, cucumbers, dressing.");
     }
 
     @Override
-    protected void startPlay() {
-        System.out.println("Football Game Started. Enjoy the game!");
+    protected void cook() {
+        System.out.println("Ingredients are chopped and mixed.");
     }
 
     @Override
-    protected void endPlay() {
-        System.out.println("Football Game Finished!");
+    protected void serve() {
+        System.out.println("Salad is served in a bowl.");
     }
 }
 
 // Demo class to test the Template Method pattern
 public class TemplateMethodPatternDemo {
     public static void main(String[] args) {
-        Game cricket = new Cricket();
-        cricket.play();
+        Recipe pasta = new PastaRecipe();
+        pasta.prepare();
 
         System.out.println();
 
-        Game football = new Football();
-        football.play();
+        Recipe salad = new SaladRecipe();
+        salad.prepare();
     }
+}
